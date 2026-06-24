f75ca8bd2a0d2a9f5831af3744f0e9b69c03f5d3
VinceMack <sentinelvhast@gmail.com>
2026-06-24 15:54:58 -0700

    Remove legacy "non-Intel" compatibility functions and related declarations.

--- src/r_local.h
+++ src/r_local.h
@@ -120,8 +120,6 @@ texture_t* R_TextureAnimation(texture_t* base);
 
 void R_GenSkyTile(void* pdest);
 void R_GenSkyTile16(void* pdest);
-void R_Surf8Patch(void);
-void R_Surf16Patch(void);
 void R_DrawSubmodelPolygons(model_t* pmodel, int clipflags);
 void R_DrawSolidClippedSubmodelPolygons(model_t* pmodel);
 
@@ -220,7 +218,6 @@ void R_DrawParticles(void);
 void R_InitParticles(void);
 void R_ClearParticles(void);
 void R_ReadPointFile_f(void);
-void R_SurfacePatch(void);
 
 extern int r_amodels_drawn;
 extern edge_t* auxedges;
